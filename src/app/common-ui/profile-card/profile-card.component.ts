43531013744d3fa18d8c1d859a2b44cf83cb8455
www-net <78052177+www-net@users.noreply.github.com>
2024-12-16 15:31:22 +0300

    feat: interpolate data into a profile card

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Profile} from '../../data/interfaces/profile.interface';
+import {NgSwitchCase} from '@angular/common';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [
+    NgSwitchCase
+  ],
   templateUrl: './profile-card.component.html',
   standalone: true,
   styleUrl: './profile-card.component.scss'
 })
 export class ProfileCardComponent {
-
+  @Input() profile!: Profile;
 }
