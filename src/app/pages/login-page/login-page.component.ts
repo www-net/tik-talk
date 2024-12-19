a23f47f2476ccd15feba2e40954fe456a0f40464
www-net <78052177+www-net@users.noreply.github.com>
2024-12-19 13:29:12 +0300

    feat: create auth service from login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   standalone: true,
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  authService = inject(AuthService)
 
+  form = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  })
+
+  onSubmit() {
+
+    if (this.form.valid) {
+      //@ts-ignore
+      this.authService.login(this.form.value).subscribe()
+    }
+  }
 }
