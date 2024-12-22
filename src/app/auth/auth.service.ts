be71a12b992156aa6dcf0eef94b03340e9cdafcb
www-net <78052177+www-net@users.noreply.github.com>
2024-12-22 08:50:05 +0300

    feat: add authentication and guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,23 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {tap} from 'rxjs';
+import {TokenResponse} from './auth.interface';
+import {CookieService} from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   http = inject(HttpClient);
-
+  cookieService = inject(CookieService)
   baseApiUrl: string = 'https://icherniakov.ru/yt-course/auth/'
+
+  token: string | null = null
+  refreshToken: string | null = null
+
+  get isAuth() {
+    return !!this.token
+  }
 
   login(payload: {username: string, password: string}) {
     const fd = new FormData()
@@ -15,9 +25,14 @@ export class AuthService {
     fd.append('username', payload.username)
     fd.append('password', payload.password)
 
-    return this.http.post(
+    return this.http.post<TokenResponse>(
       `${this.baseApiUrl}token`,
       fd,
-      )
+      ).pipe(
+        tap(val => {
+          this.token = val.access_token
+          this.refreshToken = val.refresh_token
+        })
+    )
   }
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -13,17 +14,21 @@ import {AuthService} from '../../auth/auth.service';
 })
 export class LoginPageComponent {
   authService = inject(AuthService)
+  router = inject(Router)
 
   form = new FormGroup({
-    username: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required),
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required),
   })
 
   onSubmit() {
-
     if (this.form.valid) {
       //@ts-ignore
-      this.authService.login(this.form.value).subscribe()
+      this.authService.login(this.form.value)
+        .subscribe(res => {
+          this.router.navigate([``])
+          console.log(res)
+      })
     }
   }
 }
